9b5c0d894dbb59b435d9cff42bc1f2edf2a8d03c
Егор Шиманюк <TheZebraMail@yandex.ru>
2024-01-13 19:43:27 +0100

    Latest commit

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,4 +1,127 @@
 <script>
+    import {Button} from "@ui/button"
+    import {Separator} from "@ui/separator"
+    import * as Card from "@ui/card"
+    import {ArrowRight, Plus} from "lucide-svelte"
+    import * as Carousel from "@ui/carousel"
+
+    const locations = [
+        {
+            name: "bakery1",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/30/19/enhanced/webdr09/original-28255-1404172712-10.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto",
+            location: "New York",
+        },
+        {
+            name: "bakery2",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/30/0/enhanced/webdr05/original-25738-1404101525-25.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+            location: "Tokyo",
+        },
+        {
+            name: "bakery3",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/30/19/enhanced/webdr02/original-316-1404169774-25.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+            location: "Copenhagen",
+        },
+        {
+            name: "bakery4",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/30/19/enhanced/webdr02/original-20968-1404172591-3.png?downsize=600:*&output-format=auto&output-quality=auto",
+            location: "Paris",
+        },
+        {
+            name: "bakery5",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/30/0/enhanced/webdr07/original-15362-1404101629-14.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+            location: "Milan",
+        },
+        {
+            name: "bakery6",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/27/16/enhanced/webdr11/original-20160-1403900594-31.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+            location: "Sydney",
+        },
+        {
+            name: "bakery7",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/30/0/enhanced/webdr07/original-17268-1404102391-10.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+            location: "Zurich",
+        },
+        {
+            name: "bakery8",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/30/0/enhanced/webdr06/original-28999-1404104204-16.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+            location: "Berlin",
+        },
+        {
+            name: "bakery9",
+            imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/30/18/enhanced/webdr08/original-18867-1404166118-21.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+            location: "Hong kong",
+        },
+        {
+            name: "bakery10",
+            imageUrl: "https://sp-ao.shortpixel.ai/client/to_webp,q_lossy,ret_img,w_1024/https://www.lovehappensmag.com/blog/wp-content/uploads/2021/09/Laudree-1024x673.jpg",
+            location: "Nice",
+        },
+    ]
+
 </script>
 
-<div>About us</div>
+<div class="flex gap-16">
+    <img src="https://www.magnoliabakery.com/cdn/shop/files/custom_resized_5ca7d053-1f66-455b-a77d-4115d570cf4f_1280x.jpg?v=1632848075" alt="our_bakery">
+    <div>
+        <h2 class="text-3xl font-fatface mb-4">
+            Where it all started
+        </h2>
+        <p>
+            In the heart of a charming town nestled between rolling hills and meandering rivers, Rogalik Bakery stands as a testament to the timeless artistry of baking.
+            Established in 1952 by our most passionate bakers, Rogalik Bakery has become a beloved institution, weaving a rich tapestry of flavors that have delighted generations.
+        </p>
+    </div>
+</div>
+<Separator class="my-10" />
+<div class="flex gap-16">
+    <div>
+        <h2 class="text-3xl font-fatface mb-4">
+            Our team
+        </h2>
+        <p>
+            A team of skilled pastry chefs, led by the Head Baker, works diligently to bring a variety of sweet and savory treats to life.
+            Each chef brings their unique flair to the kitchen, combining traditional techniques with modern creativity.
+            Their dedication to quality and innovation is evident in every bite of the delightful pastries offered at Rogalik Bakery.
+        </p>
+        <h3 class="text-3xl font-fatface text-center mb-4">
+            Intrested in working with us? You can apply down below:
+        </h3>
+        <Button class ="flex mx-auto"size="lg">
+            <span class="font-bold mr-2">Join our workforce</span>
+            <ArrowRight />
+        </Button>
+    </div>
+    <img src="http://klublady.ru/uploads/posts/2022-02/1644689492_14-klublady-ru-p-konditerskii-shokolad-foto-16.jpg" alt="baker">
+</div>
+
+<Separator class="my-10" />
+
+<div>
+    <h2 class="font-fatface text-5xl text-center mb-8">Our locations around the world</h2>
+    <p>
+        Rogalik Bakery's delectable creations have transcended borders, captivating taste buds across the globe.
+        What began as a quaint establishment in a charming town has now evolved into a worldwide phenomenon with Rogalik Bakery branches gracing cities on every continent.
+        Each bakery, whether nestled in the bustling streets of New York, the historic avenues of Paris, or the vibrant markets of Tokyo, carries the same commitment to quality and passion for baking that defined the original establishment. 
+    </p>
+</div>
+
+<div class="flex justify-center">
+    <Carousel.Root class="w-full max-w-xs">
+        <Carousel.Content>
+            {#each locations as bakery}
+            <Carousel.Item>
+                <div>
+                    <Card.Root>
+                        <Card.Content class="flex aspect-square items-center justify-center p-6">
+                            <img src={bakery.imageUrl} alt={bakery.name} class="block w-full rounded">
+                            <span class="text-4xl font-semibold">{bakery.location}</span>
+                        </Card.Content>
+                    </Card.Root>
+                </div>
+            </Carousel.Item>
+            {/each}
+        </Carousel.Content>
+        <Carousel.Previous />
+        <Carousel.Next />
+    </Carousel.Root>
+</div>
--- src/pages/NewOrder.svelte
+++ src/pages/NewOrder.svelte
@@ -1,1 +1,135 @@
-<div>Make a new order</div>
+<script lang="ts" context="module">
+    import {Button} from "@ui/button"
+    import {Separator} from "@ui/separator"
+    import {ArrowRight, Plus} from "lucide-svelte"
+    import * as Select from "@ui/select";
+    import { Input } from "@ui/input";
+    import { Label } from "@ui/label";
+    import { Checkbox } from "@ui/checkbox";
+    import { Textarea } from "@ui/textarea";
+    import { RangeCalendar } from "@ui/range-calendar";
+    import { today, getLocalTimeZone } from "@internationalized/date";
+
+    const start = today(getLocalTimeZone());
+    const end = start.add({ days: 7 });
+
+    let value = {
+        start,
+        end
+    };
+
+    const toppings = [
+    { value: "chocolate", label: "Additional chocolate" },
+    { value: "banana", label: "Banana" },
+    { value: "blueberry", label: "Blueberry" },
+    { value: "grapes", label: "Grapes" },
+    { value: "pineapple", label: "Pineapple" },
+    { value: "strawberry", label: "Strawberries"},
+    { value: "plazma", label: "Plazma"}
+  ];
+</script>
+
+<div class="flex justify-center">
+    <div>
+        <h3 class="text-3xl font-fatface mb-4">
+            Order a custom cake
+        </h3>
+        <p>
+            Want to order a custom cake? Fill the form down below with your desired details:
+        </p>
+    </div>
+</div>
+
+<Separator class="my-10" />
+
+<div class="flex justify-center gap-16">
+    <form>
+        <div>
+            <Label for="name">
+                Name
+            </Label>
+            <Input required id="name" type="text" placeholder="Name"/>
+        </div>
+        
+        <div>
+            <Label for="last_name">
+                Last name
+            </Label>
+            <Input required id="last_name" type="text" placeholder="Last Name"/>
+        </div>
+        
+        <div>
+            <Label for="email">
+                E-mail
+            </Label>
+            <Input required id="email" type="email" placeholder="user@mail.com"/>
+        </div>
+        
+        <div>
+            <Label for="base_topping">
+                Choose a base for your cake
+            </Label>
+            <Input type="radio" id="cream" name="base" value="cream"/>
+            <Label for="cream">Cream</Label>
+            <Input type="radio" id="chocolate" name="base" value="chocolate"/>
+            <Label for="chocolate">Chocolate</Label>
+            <Input type="radio" id="pancake" name="base" value="pancake"/>
+            <Label for="pancake">Pancake</Label>
+            <Input type="radio" id="coffee" name="base" value="coffee"/>
+            <Label for="coffee">Coffee</Label>
+        </div>
+        
+        <div>
+            <Label>
+                Choose the toppings for your cake
+            </Label>
+            <Select.Root portal={null}>
+                <Select.Trigger class="w-[180px]">
+                <Select.Value placeholder="Select a topping" />
+                </Select.Trigger>
+                <Select.Content>
+                <Select.Group>
+                    {#each toppings as topping}
+                    <Select.Item value={topping.value} label={topping.label}>
+                        {topping.label}
+                    </Select.Item>
+                    {/each}
+                </Select.Group>
+                </Select.Content>
+                <Select.Input name="favoriteFruit" />
+            </Select.Root>
+        </div>
+        
+        <div>
+            <Label>
+                Choose a date of delivery
+            </Label>
+            <RangeCalendar bind:value class="border rounded-md" />
+        </div>
+        
+        <div>
+            <Checkbox id="box"/>
+            <Label for="box">
+                Deliver the cake in a wrapped box
+            </Label>
+        </div>
+        
+        <div>
+            <Label for="picture">Leave a reference photo</Label>
+            <Input id="picture" type="file" />
+        </div>
+        
+        <div>
+            <Label for="text">
+                Leave your additional info for us here!
+            </Label>
+            <Textarea id="text" placeholder="Comments"/>
+        </div>
+        
+
+        <Button class="flex mx-auto" size="lg" type="submit">
+            <span class="font-bold mr-2">Submit</span>
+            <ArrowRight />
+        </Button>
+    </form>
+</div>
