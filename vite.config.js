736b3c0e9ba16ae1eecde895e6ce53681c70d986
Nikita Troshnev <nik.troshnev@gmail.com>
2024-01-25 16:03:21 +0000

    feat: remove hardcoded repo name

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from "url";
+import { fileURLToPath, URL } from "url"
 
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import {webfontDownload} from "vite-plugin-webfont-dl";
+import { defineConfig } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+import { webfontDownload } from "vite-plugin-webfont-dl"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     svelte(),
     webfontDownload([
-      'https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap'
+      "https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
     ])
   ],
   resolve: {
@@ -19,5 +19,5 @@ export default defineConfig({
       $lib: fileURLToPath(new URL("./src/lib", import.meta.url))
     }
   },
-  base: '/RogalikLanding/'
-})
+  base: mode === "production" ? process.env.NODE_BASE_PATH : undefined
+}))
