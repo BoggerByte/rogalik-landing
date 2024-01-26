a47f97233a0696309476289772c63649c49c5747
Nikita Troshnev <nik.troshnev@gmail.com>
2024-01-26 00:35:55 +0000

    feat: Migrated to SvelteKit with static adapter

---
+++ src/hooks.js
@@ -0,0 +1,5 @@
+export const handle = async ({ event, resolve }) => {
+  return await resolve(event, {
+    ssr: false
+  })
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import "@/app.css"
+
+  import { ModeWatcher } from "mode-watcher"
+  import NavBar from "@/lib/components/NavBar.svelte"
+  import Footer from "@/lib/components/Footer.svelte"
+  import { Toaster } from "@ui/sonner"
+</script>
+
+<div class="relative">
+  <header class="absolute w-full">
+    <NavBar />
+  </header>
+
+  <main class="container mx-auto min-h-screen pb-[200px] pt-[80px]">
+    <slot />
+  </main>
+
+  <footer class="absolute bottom-0 w-full">
+    <Footer />
+  </footer>
+</div>
+
+<ModeWatcher />
+<Toaster richColors />
